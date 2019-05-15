408ae65f22bc9d8152a84260c705247593785939
Ryan Smith <ryan.l.smith4@gmail.com>
2019-05-15 13:15:14 -0700

    [update] scenario 2

--- scenario_two.py
+++ scenario_two.py
@@ -3,19 +3,23 @@ from helper import read_str_dict, contains
 
 # ! Overall runtime = O(n)
 # ! Overall memory = O(n)
+def read_file(file):
+    number_list =  []
+    with open(file) as f:
+        for number in f.read().splitlines():
+            number_list.append(number)
+    return number_list
 
-def route_cost_check(phone_num: [str], route_cost_csv) -> str:
-    # TODO: refactor to allow list of phone num
+def route_cost_check(phone_num: str, prefix_and_route_costs) -> str:
     
     '''cost variable updates as we iteratave over the dicitonary according too
     matching prefix if it is the lowest offset'''
 
     # This returns a dictionary of the prefixes and the cost as (key, value) pairs
-    prefix_and_route_costs = read_str_dict(route_cost_csv) 
+    
     cost = None
     # arbitrary large value for beginning the loop
     old_offset = 2 ** 1000
-
     # loop through prefix keys in dict
     for prefix in prefix_and_route_costs.keys():
         # ! Runtime = O(n), n being the number of keys in prefix dict
@@ -33,6 +37,20 @@ def route_cost_check(phone_num: [str], route_cost_csv) -> str:
         return None
 
     return cost
- 
-# print(route_cost_check('+34924199345454', 'route-costs-600.txt'))
+
+def lots_of_numbers():
+    numbers = read_file('phone-numbers-1000.txt')
+    price_list = []
+    prefix_and_route_costs = read_str_dict('route-costs-35000.txt') 
+    for number in numbers:
+        cost = route_cost_check(number, prefix_and_route_costs)
+        if cost is not None:
+            price_list.append((number, cost))
+    return price_list
+
+
+# numbers = read_file('phone-numbers-1000.txt')
+print(lots_of_numbers())
+# print(route_cost_check('', 'route-costs-35000.txt'))
+# print(read_file('phone-numbers-1000.txt'))
 
